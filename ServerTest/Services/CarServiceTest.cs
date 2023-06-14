0f7e030a49135750b73e98f6d39b18ec782a9aa8
Timo Zink <zink.timo@protonmail.com>
2023-06-14 21:29:01 +0200

    new Test added

---
+++ ServerTest/Services/CarServiceTest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fuel.Manager.Server.Models;
+using Fuel.Manager.Server.Repositories.Interfaces;
+using Fuel.Manager.Server.Services.Implementation;
+using Moq;
+
+namespace ServerTest.Services
+{
+
+    [TestClass]
+    public class CarServiceTests
+    {
+        private Mock<ICarRepository> _carRepositoryMock;
+        private CarService _carService;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _carRepositoryMock = new Mock<ICarRepository>();
+            _carService = new CarService(_carRepositoryMock.Object);
+        }
+
+        [TestMethod]
+        public void GetAll_ReturnsAllCars()
+        {
+            // Arrange
+            var cars = new List<Car>
+        {
+            new Car { Id = 1, Vendor = "Toyota", Model = "Corolla" },
+            new Car { Id = 2, Vendor = "Honda", Model = "Civic" }
+        };
+            _carRepositoryMock.Setup(x => x.GetAll()).Returns(cars);
+
+            // Act
+            var result = _carService.GetAll();
+
+            // Assert
+            CollectionAssert.AreEqual(cars, (ICollection)result);
+        }
+
+        [TestMethod]
+        public void GetById_ReturnsCarWithMatchingId()
+        {
+            // Arrange
+            var carId = 1;
+            var car = new Car { Id = carId, Vendor = "Toyota", Model = "Corolla" };
+            _carRepositoryMock.Setup(x => x.GetById(carId)).Returns(car);
+
+            // Act
+            var result = _carService.GetById(carId);
+
+            // Assert
+            Assert.AreEqual(car, result);
+        }
+
+        [TestMethod]
+        public void Create_CallsCarRepositoryCreate()
+        {
+            // Arrange
+            var car = new Car { Id = 1, Vendor = "Toyota", Model = "Corolla" };
+
+            // Act
+            _carService.Create(car);
+
+            // Assert
+            _carRepositoryMock.Verify(x => x.Create(car), Times.Once);
+        }
+
+        [TestMethod]
+        public void Update_CallsCarRepositoryUpdate()
+        {
+            // Arrange
+            var car = new Car { Id = 1, Vendor = "Toyota", Model = "Corolla" };
+
+            // Act
+            _carService.Update(car);
+
+            // Assert
+            _carRepositoryMock.Verify(x => x.Update(car), Times.Once);
+        }
+
+        [TestMethod]
+        public void Delete_CallsCarRepositoryDelete()
+        {
+            // Arrange
+            var car = new Car { Id = 1, Vendor = "Toyota", Model = "Corolla" };
+
+            // Act
+            _carService.Delete(car);
+
+            // Assert
+            _carRepositoryMock.Verify(x => x.Delete(car), Times.Once);
+        }
+    }
+
+}
--- ServerTest/Services/EmployeeServiceTest.cs
+++ ServerTest/Services/EmployeeServiceTest.cs
@@ -1,16 +1,138 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
+using Fuel.Manager.Server.Models;
+using Fuel.Manager.Server.Repositories.Interfaces;
+using Fuel.Manager.Server.Services.Implementation;
+using Moq;
 
 namespace ServerTest.Services
 {
-    [TestClass]
-    public class EmployeeServiceTest
-    {
 
-        //LoginTest-1
-        
+
+    [TestClass]
+    public class EmployeeServiceTests
+    {
+        private Mock<IEmployeeRepository> _employeeRepositoryMock;
+        private EmployeeService _employeeService;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _employeeRepositoryMock = new Mock<IEmployeeRepository>();
+            _employeeService = new EmployeeService(_employeeRepositoryMock.Object);
+        }
+
+        [TestMethod]
+        public void GetAll_ReturnsAllEmployees()
+        {
+            // Arrange
+            var employees = new List<Employee>
+            {
+                new Employee {
+                    Id = 1,
+                    Username = "Hans",
+                    Lastname = "Peter",
+                    EmployeeNo = "1PCR9",
+                    IsAdmin = true,
+                },
+                new Employee {
+                    Id = 1,
+                    Username = "Joachim",
+                    Lastname = "Schnell",
+                    EmployeeNo = "1234",
+                    IsAdmin = true,
+                },
+            };
+            _employeeRepositoryMock.Setup(x => x.GetAll()).Returns(employees);
+
+            // Act
+            var result = _employeeService.GetAll();
+
+            // Assert
+            CollectionAssert.AreEqual(employees, (ICollection)result);
+        }
+
+        [TestMethod]
+        public void GetById_ReturnsEmployeeWithMatchingId()
+        {
+            // Arrange
+            var employeeId = 1;
+            var employee = new Employee
+            {
+                Id = 1,
+                Username = "Hans",
+                Lastname = "Peter",
+                EmployeeNo = "1PCR9",
+                IsAdmin = true,
+            };
+
+            _employeeRepositoryMock.Setup(x => x.GetById(employeeId)).Returns(employee);
+
+            // Act
+            var result = _employeeService.GetById(employeeId);
+
+            // Assert
+            Assert.AreEqual(employee, result);
+        }
+
+        [TestMethod]
+        public void Create_CallsEmployeeRepositoryCreate()
+        {
+            // Arrange
+            var employee = new Employee
+            {
+                Id = 1,
+                Username = "Hans",
+                Lastname = "Peter",
+                EmployeeNo = "1PCR9",
+                IsAdmin = true,
+            };
+
+            // Act
+            _employeeService.Create(employee);
+
+            // Assert
+            _employeeRepositoryMock.Verify(x => x.Create(employee), Times.Once);
+        }
+
+        [TestMethod]
+        public void Update_CallsEmployeeRepositoryUpdate()
+        {
+            // Arrange
+            var employee = new Employee
+            {
+                Id = 1,
+                Username = "Hans",
+                Lastname = "Peter",
+                EmployeeNo = "1PCR9",
+                IsAdmin = true,
+            };
+
+            // Act
+            _employeeService.Update(employee);
+
+            // Assert
+            _employeeRepositoryMock.Verify(x => x.Update(employee), Times.Once);
+        }
+
+        [TestMethod]
+        public void Delete_CallsEmployeeRepositoryDelete()
+        {
+            // Arrange
+            var employee = new Employee
+            {
+                Id = 1,
+                Username = "Hans",
+                Lastname = "Peter",
+                EmployeeNo = "1PCR9",
+                IsAdmin = true,
+            };
+
+            // Act
+            _employeeService.Delete(employee);
+
+            // Assert
+            _employeeRepositoryMock.Verify(x => x.Delete(employee), Times.Once);
+        }
     }
+
 }
